7ba5785c94a1869f8461e15a23c247792bf5ee74
Bradley Robles <bradleyrobles88@gmail.com>
2022-10-01 22:51:10 -0500

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,11 @@
+#Parent Class
+class Instrument:
+    Brand =''
+    color =''
+#Children Classes of Insturment class
+class Piano(Instrument):
+    Key_Count = 88
+    Key_Weight = '30 grams' 
+class Guitar(Instrument):
+    String_Count = 6
+    Tuning = 'EADGBE'    
