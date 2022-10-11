93d23235a571ab05a1bd5103642c5439117fa577
Bradley Robles <bradleyrobles88@gmail.com>
2022-10-10 19:08:37 -0500

    Lines 70-79 checks if files are new or modified in last 24 hours

--- File Transfer Assignment/fIle_transfer.py
+++ File Transfer Assignment/fIle_transfer.py
@@ -1,7 +1,13 @@
 
+from sqlite3 import Timestamp
 import tkinter as tk
 from tkinter import *
 import tkinter.filedialog
+import os
+from os import *
+import shutil
+from datetime import *
+
 
 class ParentWindow (Frame):
     def __init__(self, master):
@@ -9,7 +15,7 @@ class ParentWindow (Frame):
         #Sets title of GUI Window
         self.master.title("File Transfer")
         #Creates button to select files from source directory
-        self.sourceDir_btn = Button(text="Select Source",width=20)
+        self.sourceDir_btn = Button(text="Select Source",width=20, command=self.sourceDir)
         #Positions source button in GUI using tkinter grid()
         self.sourceDir_btn.grid(row=0, column=0, padx=(20, 10), pady=(30, 0))
 
@@ -20,7 +26,7 @@ class ParentWindow (Frame):
         self.source_dir.grid(row=0, column=1, columnspan=2, padx=(20,10), pady = (30,0))
 
         #Creates button to select destination of files from destination directory
-        self.destDir_btn = Button(text="Select Destination", width=20)
+        self.destDir_btn = Button(text="Select Destination", width=20, command=self.destDir)
         #Positions destination button in GUI using tkinter grid()
         #on the next row under the source button
         self.destDir_btn.grid(row=1, column=0, padx=(20, 10), pady=(15,10))
@@ -31,25 +37,56 @@ class ParentWindow (Frame):
         #the button to ensure they will line up
         self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15,10))
 
+        #Creates files to transfer files
+        self.transfer_btn = Button (text="Transfer Files", width=20, command=self.transferFiles)
+        #Positions transfer files button
+        self.transfer_btn.grid(row=2, column=1, padx=(200, 0), pady=(0,15))
+
+        #Creates an Exit Button
+        self.exit_btn = Button (text="Exit", width= 20, command=self.exit_program)
+        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))
+
+    def sourceDir(self):
+        selectSourceDir = tkinter.filedialog.askdirectory()
+        #The .delete (0, END) will clear the content that is inserted in the Entry widget,
+        #this allows the path to be inserted into  the Entry widget properly.
+        self.source_dir.delete(0,END)
+        #The .insert method will insert the user selection to the source_dir Entry
+        self.source_dir.insert(0, selectSourceDir)
+
+    #Creates function to select destination directory.
+    def destDir(self):
+        selectDestDir= tkinter.filedialog.askdirectory()
+        self.destination_dir.delete(0, END)
+        self.destination_dir.insert(0, selectDestDir)
+    
+    #Creates function to transfer files from one directory to another
+    def transferFiles(self):
+        source=self.source_dir.get()
+        destination = self.destination_dir.get()
+        source_files = os.listdir(source)
+        
+        #Checks if files are new or modified in last 24 hours
+        date_curr = datetime.now()#Var for current time
+        date_24hr = date_curr - timedelta(hours=24)#Var for time 24 hours ago
+        path = self.source_dir.get()# Path to source folder
+        for i in source_files:
+            fullpath = os.path.join(path,i)#joins path to folder and files
+            date_DM = os.path.getmtime(fullpath)# Gets date modified for each file
+            date_eng = datetime.fromtimestamp(date_DM)#converts date_DM from secs to readable format ("eng")
+            if date_eng > date_24hr: #transfers files if files modif. date is > past 24s
+                shutil.move(source + '/' + i, destination)
+                print(i + ' was sucessfully transferred.' )
+
+    def exit_program(self):
+        #root is the main GUI window, the Tkinter destory method
+        #tells python to terminate root.mainloop and all widgets inside the GUI window
+        root.destroy()
+
+
+
+
 #Creates function to select source directory.
-def sourceDir(self):
-    selectSourceDir = tkinter.fileddialog.askdirectory()
-    #The .delete (0, END) will clear the content that is inserted in the Entry widget,
-    #this allows the path to be inserted into  the Entry widget properly.
-    self.source_dir.delete(0,END)
-    #The .insert method will insert the user selection to the source_dir Entry
-    self.source_dir.insert(0, selectSourceDir)
-
-    #Creates button to select files from source directory
-    self.sourceDir_button = Button(text="Select Source", width=20, command=self.sourceDir)
-
-#Creates function to select destination directory.
-def destDir(self):
-    selectDestDir= tkinter.filedialog.askdirectory()
-    self.destination_dir.delete(0, END)
-    self.destination_dir.insert(0, selectDestDir)
-
-    self.destDir_btn = Button("Select Destination", width=20, command=self.destDir)
 
 if __name__ == "__main__":
     root = tk.Tk()
