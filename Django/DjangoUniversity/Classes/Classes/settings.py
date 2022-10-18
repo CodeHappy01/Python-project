01a535a9f9ec199136e1e953115fb84e4b4339ac
Bradley Robles <bradleyrobles88@gmail.com>
2022-10-18 16:02:06 -0500

    Update settings.py

--- Django/DjangoUniversity/Classes/Classes/settings.py
+++ Django/DjangoUniversity/Classes/Classes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^@!326z+#ivqq#@e)^$sh29e%qglp_mne0+#ko8=v*w3(_%8&p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
