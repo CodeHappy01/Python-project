0a01addb1a2eb993240db8de67c729e5e8d51601
Bradley Robles <bradleyrobles88@gmail.com>
2022-10-19 10:50:48 -0500

    Update settings.py

--- Django/DjangoCheckbook/BlueBirdBanking/BlueBirdBanking/settings.py
+++ Django/DjangoCheckbook/BlueBirdBanking/BlueBirdBanking/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jgagd2=gznwp(8oh31^g3kww*_udid-0&!$t(xm()ax+#hv4*d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
